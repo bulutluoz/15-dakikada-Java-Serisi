68c8276b63b08a396038e9856361ddd3d6781cb2
bulutluoz <abbulutluoz@gmail.com>
2022-04-08 10:00:37 +0200

    inheritance

---
+++ src/inheritance/BaseClass.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class BaseClass {
+    protected static int sayi=10;
+    protected String str="Ali";
+
+    public static void method1(){
+        // Codes
+    }
+
+    public void method2(){
+        // Codes
+    }
+
+}
---
+++ src/inheritance/ChildOfBaseClass1.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class ChildOfBaseClass1 extends BaseClass{
+    public static void main(String[] args) {
+        sayi=20;
+        method1();
+
+        ChildOfBaseClass1 obj=new ChildOfBaseClass1();
+        obj.str="Can";
+        obj.method2();
+    }}
+
+
