d1cc0e74256e483f7e39a00748a3d0cbba69b67d
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-07-11 17:34:19 +0200

    Array eklendi

---
+++ src/javaGiris/HelloWorld.java
@@ -0,0 +1,28 @@
+package javaGiris;
+
+public class HelloWorld {
+    int yas=25;
+    String isim="Ali Can";
+
+
+    public static void main(String[] args) {
+        boolean emekliMi=false;
+        double diplomaNotu=78.3;
+
+
+        System.out.println("Hello World");
+    }
+
+    public void toplama(int sayi1, int sayi2){
+        System.out.println(sayi1+sayi2);
+    }
+}
+
+
+
+
+
+
+
+
+
